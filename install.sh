85e4fd652e094d6f5b3fe8ac092152bcc1a324db
Jonas L. Holm <duch11@users.noreply.github.com>
2022-03-28 18:14:34 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
+
+set -euxo pipefail
+
+ls /usr/share/icons/Adwaita
+
+echo "creating .icons/ folder.."
 mkdir -p $HOME/.icons
+echo "Copy System Adwaita icons to .icons/ folder.."
 cp -r /usr/share/icons/Adwaita $HOME/.icons/Adwaita
+echo "Overwriting Adwaita folder icons.."
 cp -r ./Adwaita-blue/. $HOME/.icons/Adwaita
